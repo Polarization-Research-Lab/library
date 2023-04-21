a848fd093e5109af8ba502cf83277d019b2b424f
m <m@ms-MacBook-Pro.local>
2023-04-21 16:24:59 -0400

    added run-all build script (pipeline.py)

---
+++ .build/pipeline.py
@@ -0,0 +1,3 @@
+with open('push from google sheets to db.py', 'r') as file: exec(file.read())
+with open('pull glossary from google sheets.py', 'r') as file: exec(file.read())
+with open('pull entries from db.py', 'r') as file: exec(file.read())
